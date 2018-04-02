a87ced073b29796691ef56ed0063c3d39af6f8b0
davidrjonas <davidrjonas@users.noreply.github.com>
2018-04-02 01:30:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+semver-cli
+==========
+
+[![Build Status](https://travis-ci.org/davidrjonas/semver-cli)](https://travis-ci.org/davidrjonas/semver-cli)
+[![Go Report Card](https://goreportcard.com/badge/github.com/davidrjonas/semver-cli)](https://goreportcard.com/report/github.com/davidrjonas/semver-cli)
+
+semver-cli is a simple command line tool to compare and manipulate version strings.
+
+It is basically a cli wrapper around the excellent [Masterminds semver library](https://github.com/Masterminds/semver), with lots of help from the also excellent [Kingpin](https://github.com/alecthomas/kingpin)
+
+```
+usage: semver [<flags>] <command> [<args> ...]
+
+Command-line semver tools. On error, print to stderr and exit -1.
+
+Flags:
+      --help     Show context-sensitive help (also try --help-long and --help-man).
+  -v, --verbose  Verbose mode.
+
+Commands:
+  help [<command>...]
+    Show help.
+
+  satisfies <VERSION> <CONSTRAINTS>
+    Test if a version satisfies a constraint. Exit 0 if satisfies, 1 if not. If verbose, print an explanation to stdout.
+
+  greater <A> <B>
+    Compare two versions. Exit 0 if the first is greater, 1 if not. If verbose, print greater to stdout.
+
+  lesser <A> <B>
+    Compare two versions. Exit 0 if the first is lesser, 1 if not. If verbose, print lesser to stdout.
+
+  equal <A> <B>
+    Compare two versions. Exit 0 if they are equal, 1 if not.
+
+  inc <COMPONENT> <VERSION>
+    Increment major, minor, or patch component.
+
+  set <COMPONENT> <VERSION> <VALUE>
+    Set prerelease or metadata component.
+```
+
+Example
+-------
+
+Deploy only when the CI tag is within constraints and is greater than what is currently released. For example, with constraints 1.* and a released version of 1.4, a version of 1.5 would be released but a version of 2.0 or 1.2 would not.
+
+```bash
+#!/bin/bash
+
+CONSTRAINTS=$(jq .labels.constraints manifest.json)
+RELEASED=$(jq .image manifest.json | cut -d: -f 2)
+
+semver satisfies "$CI_TAG" "$CONSTRAINTS" || exit 1
+semver greater   "$CI_TAG" "$RELEASED"    || exit 1
+
+deploy
+```
---
+++ main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Masterminds/semver"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var version = "1.0.0"
+
+var (
+	app     = kingpin.New("semver", "Command-line semver tools. On error, print to stderr and exit -1.")
+	verbose = app.Flag("verbose", "Verbose mode.").Short('v').Bool()
+
+	satisfies            = app.Command("satisfies", "Test if a version satisfies a constraint. Exit 0 if satisfies, 1 if not. If verbose, print an explanation to stdout.")
+	satisfiesVersion     = satisfies.Arg("VERSION", "The version to test").Required().String()
+	satisfiesConstraints = satisfies.Arg("CONSTRAINTS", "The constraints to test against").Required().String()
+
+	greater  = app.Command("greater", "Compare two versions. Exit 0 if the first is greater, 1 if not. If verbose, print greater to stdout.")
+	greaterA = greater.Arg("A", "Left side of A > B").Required().String()
+	greaterB = greater.Arg("B", "Right side of A > B").Required().String()
+
+	lesser  = app.Command("lesser", "Compare two versions. Exit 0 if the first is lesser, 1 if not. If verbose, print lesser to stdout.")
+	lesserA = lesser.Arg("A", "Left side of A < B").Required().String()
+	lesserB = lesser.Arg("B", "Right side of A < B").Required().String()
+
+	equal  = app.Command("equal", "Compare two versions. Exit 0 if they are equal, 1 if not.")
+	equalA = equal.Arg("A", "Left side of A = B").Required().String()
+	equalB = equal.Arg("B", "Right side of A = B").Required().String()
+
+	inc          = app.Command("inc", "Increment major, minor, or patch component.")
+	incComponent = inc.Arg("COMPONENT", "The component to increment. Possible values: [major, minor, patch]").Required().String()
+	incVersion   = inc.Arg("VERSION", "The version to increment.").Required().String()
+
+	set          = app.Command("set", "Set prerelease or metadata component.")
+	setComponent = set.Arg("COMPONENT", "The component to increment. Possible values: [prerelease, metadata]").Required().String()
+	setVersion   = set.Arg("VERSION", "The version of which to set a component.").Required().String()
+	setValue     = set.Arg("VALUE", "The value to set.").Required().String()
+)
+
+func main() {
+	kingpin.Version(version)
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case satisfies.FullCommand():
+		v := mustParseVersion(*satisfiesVersion, "VERSION")
+		c := mustParseConstraints(*satisfiesConstraints)
+
+		if does, msgs := c.Validate(v); !does {
+			if *verbose {
+				for _, m := range msgs {
+					fmt.Println(m)
+				}
+			}
+
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+
+	case greater.FullCommand():
+		a := mustParseVersion(*greaterA, "A")
+		b := mustParseVersion(*greaterB, "B")
+
+		if !a.GreaterThan(b) {
+			if *verbose {
+				fmt.Println(*greaterB)
+			}
+			os.Exit(1)
+		}
+
+		if *verbose {
+			fmt.Println(*greaterA)
+		}
+		os.Exit(0)
+
+	case lesser.FullCommand():
+		a := mustParseVersion(*lesserA, "A")
+		b := mustParseVersion(*lesserB, "B")
+
+		if !a.LessThan(b) {
+			if *verbose {
+				fmt.Println(*lesserB)
+			}
+			os.Exit(1)
+		}
+
+		if *verbose {
+			fmt.Println(*lesserA)
+		}
+		os.Exit(0)
+
+	case equal.FullCommand():
+		a := mustParseVersion(*equalA, "A")
+		b := mustParseVersion(*equalB, "B")
+
+		if !a.Equal(b) {
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+
+	case inc.FullCommand():
+		v := mustParseVersion(*incVersion, "VERSION")
+		var v1 semver.Version
+		switch *incComponent {
+		case "major":
+			v1 = v.IncMajor()
+		case "minor":
+			v1 = v.IncMinor()
+		case "patch":
+			v1 = v.IncPatch()
+		default:
+			fmt.Fprintf(os.Stderr, "unknown component name: '%s'\n", *incComponent)
+			os.Exit(-1)
+		}
+		fmt.Println(v1.String())
+
+	case set.FullCommand():
+		v := mustParseVersion(*setVersion, "VERSION")
+		var v1 semver.Version
+		var err error
+		switch *setComponent {
+		case "prerelease":
+			if v1, err = v.SetPrerelease(*setValue); err != nil {
+				fmt.Fprintf(os.Stderr, "invalid prerelease; %v\n", err)
+				os.Exit(-1)
+			}
+		case "metadata":
+			if v1, err = v.SetMetadata(*setValue); err != nil {
+				fmt.Fprintf(os.Stderr, "invalid metadata; %v\n", err)
+				os.Exit(-1)
+			}
+		default:
+			fmt.Fprintf(os.Stderr, "unknown component name: '%s'\n", *setComponent)
+			os.Exit(-1)
+		}
+		fmt.Println(v1.String())
+	}
+}
+
+func mustParseVersion(s, ctx string) *semver.Version {
+	v, err := semver.NewVersion(s)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse <%s> version; %v: '%s'\n", ctx, err, s)
+		os.Exit(-1)
+	}
+
+	return v
+}
+
+func mustParseConstraints(s string) *semver.Constraints {
+	c, err := semver.NewConstraint(s)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse constraints; %v\n", err)
+		os.Exit(-1)
+	}
+
+	return c
+}
